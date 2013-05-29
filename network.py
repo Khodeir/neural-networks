0e5d286237533b29902126f2e6449a43909f0908
Mohamed Khodeir <m.khodeir@mail.utoronto.ca>
2013-05-30 00:31:14 +0200

    Made RBM a subclass of NeuralNet

--- network.py
+++ network.py
@@ -15,6 +15,7 @@ class NeuralNet(object):
             self.weights.append(2*epsilon*random.rand(layer_sizes[i], layer_sizes[i+1]) - epsilon)
 
     def forward_pass(self, data):
+        '''The forward pass passes information into the bottom layer of the net and propagates the data up the net'''
         last = len(self.weights)
         for i in range(self.numlayers):
             data = self.layers[i].process(data)
@@ -22,8 +23,39 @@ class NeuralNet(object):
                 data = dot(data, self.weights[i])
         return [layer.activities for layer in self.layers]
 
+    def backward_pass(self, data):
+        '''The backward pass pretends that data is the state of the top (output) layer and propagates it down the net'''
+        assert False, "Unimplemented"
+        last = len(self.weights)
+        for i in range(last-1,-1,-1):
+            data = dot(data, self.weights[i].transpose())
+            data = self.layers[i].process(data)
+        return [layer.activities for layer in self.layers]
+
+
     def get_layers(self):
         return self.layers
 
     def get_weights(self):
         return self.weights
+
+    def feature_map(self, i, (featN, featM), (mapN, mapM)):
+        '''Returns a map of the i'th weight matrix in the network. Each unit in the i+1'th layer corresponds to a
+        (featN x featM = layer[i].size) map and there are (mapN x mapM = layer[i+1].size) maps.'''
+        assert self.layers[i].size == featN * featM, 'Number of units in the ith layer must equal featN*featM'
+        assert self.layers[i+1].size == mapN * mapM, 'Number of units in the i+1th layer must equal mapN*mapM'
+        vh = transpose(self.weights[i])
+        result = zeros((featN*mapN, featM*mapM))
+        row = 0
+        col = 0
+        for i in range(self.layers[i+1].size):
+            result[row:row+featN, col:col+featM] = vh[i].reshape((featN, featM))
+            col += featM
+            if col == featM*mapM:
+                row += featN
+                col = 0
+        return result
+
+def draw(data):
+    plt.imshow(data, cmap=plt.get_cmap('gray'))
+    plt.show()
