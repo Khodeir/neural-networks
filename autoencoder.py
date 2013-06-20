c352f31509a78f37191a36cc412955278a8d0f71
Omar Makhlouf <omar.makhlouf93@gmail.com>
2013-06-20 11:14:17 +0200

    Added fine-tuning method. Actually works pretty well.

--- autoencoder.py
+++ autoencoder.py
@@ -25,7 +25,7 @@ class AutoEncoder(object):
             third_layer = LogisticLayer(next_layer.size)
             current = NeuralNet([next_layer, architecture[i+1], third_layer])
             print 'Training ', current.layers[0].size, ' - ', current.layers[1].size, ' encoder'
-            error = train_shallow(current, next_input)
+            error = train_ae(current, next_input)
             #train_errors.append(error)
 
             next_layer = current.layers[1] #Get middle layer of this shallow autoencoder
@@ -48,14 +48,37 @@ class AutoEncoder(object):
         '''Decode data and returns it.'''
         return self.decoder.forward_pass(data,1)[-1] #Need to verify this
 
-    def fine_tune(self, data):
+    def fine_tune(self, training_inputs):
         '''Fine Tune a deep autoencoder using backprop on the whole net after it's been pre-trained'''
+        fine_layers, fine_weights = [],[]
+
+        for layer in self.encoder.layers:
+            fine_layers.append(layer)
+        for matrix in self.encoder.weights:
+            fine_weights.append(matrix)
+
+        for i in range(self.decoder.numlayers -1):
+            fine_layers.append(self.decoder.layers[i+1])
+        for matrix in self.decoder.weights:
+            fine_weights.append(matrix)
+
+        fine_net = NeuralNet(fine_layers, fine_weights)
+
+        err = train_ae(fine_net, training_inputs, epochs=5, learning_rate=0.05, decay_rate=0)
+        encode_weights, decode_weights = [],[]
+        for i in range(0,(len(fine_net.weights)/2)):
+            encode_weights.append(fine_net.weights[i])
+        for i in range((len(fine_net.weights)/2),len(fine_net.weights)):
+            decode_weights.append(fine_net.weights[i])
+
+        self.encoder.set_weights(encode_weights)
+        self.decoder.set_weights(decode_weights)
 
     def insert_layer(self, index, layer):
         '''Adds an intermediate encoding/decoding layer to the autoencoder at the given index'''
 
-def train_shallow(shallow_ae, training_inputs, epochs=5, learning_rate=0.1, batch_size=100, noise=0, decay_rate=0, get_error=False):
-    '''Trains a shallow autoencoder on data.'''
+def train_ae(ae, training_inputs, epochs=5, learning_rate=0.1, batch_size=100, noise=0, decay_rate=0, get_error=False):
+    '''Trains an assembled autoencoder on data.'''
     training_inputs = dropout(training_inputs, noise) #Add noise to input if you want
     input_batches = [training_inputs[i:i+batch_size] for i in range(0, 60000, batch_size)] #Split to mini-batches
     errors = []
@@ -63,9 +86,9 @@ def train_shallow(shallow_ae, training_inputs, epochs=5, learning_rate=0.1, batc
     for epoch in range(epochs):
         print 'Epoch Number: ', epoch
         for i in range(len(input_batches)):
-            train(shallow_ae, input_batches[i], input_batches[i], learning_rate, decay_rate)
+            train(ae, input_batches[i], input_batches[i], learning_rate, decay_rate)
             if i%100 == 0 and get_error:
-                e = error(shallow_ae, input_batches[i], input_batches[i], decay_rate)
+                e = error(ae, input_batches[i], input_batches[i], decay_rate)
                 print e
                 errors.append(e)
     if get_error:
