5da0f40c53b73123349a60ede6a256df1e0c8469
Mohamed Khodeir <m.khodeir@mail.utoronto.ca>
2014-10-31 14:34:27 -0400

    Added biases to wake-sleep

--- dbn.py
+++ dbn.py
@@ -7,8 +7,9 @@ class BN(object):
     def __init__(self, layers, up_weights=None, down_weights=None):
         '''Initializes a BN from the layers given'''
         self.numlayers = len(layers)
+        down_layers = list(layers[::-1])#Copy the list so that upnet and downnet layers are different objects
         self.upnet = NeuralNet(layers, up_weights)
-        self.downnet = NeuralNet(layers[::-1], down_weights)
+        self.downnet = NeuralNet(down_layers, down_weights)
 
     @classmethod
     def from_rbms(cls, rbms):
@@ -61,8 +62,8 @@ class BN(object):
 
     def wake_phase(self, data):
         '''The first step of wake-sleep and contrastive wake-sleep. Returns wake_deltas, a list of matrices by which the
-        the weights of the down net should be adjusted. Also returns hidden states of top layer. Assumes DBN layers are 
-        binary stochastic layers.'''
+        the weights of the down net should be adjusted. Also returns wake_bias_deltas, wake_visbias_delta, and hidden states 
+        of top layer. Assumes DBN layers are binary stochastic layers.'''
         #Get the states and probabilities of every layer after doing a bottom-up pass
         hid_states = self.bottom_up(data)
         hid_probs = []
@@ -70,21 +71,27 @@ class BN(object):
             hid_probs.append(layer.probs)
 
         wake_deltas = []
+        wake_bias_deltas = []
+        #Bias deltas for the generative visible units
+        wake_visbias_delta = (data - self.upnet.layers[0].probs).sum(0)/data.shape[0]
         #Iterate over each layer excluding bottom layer
         for i in range(self.upnet.numlayers - 1):
             upper_state = hid_states[i+1]
+            upper_activity = hid_probs[i+1]
             lower_state = hid_states[i]
             lower_activity = hid_probs[i]
 
             delta = dot(upper_state.transpose(), (lower_state - lower_activity))/data.shape[0]
-
+            #Get bias deltas as well to update hidden biases in downnet
+            delta_bias = array([(upper_state - upper_activity).sum(0)/data.shape[0]])
             wake_deltas.insert(0,delta)
+            wake_bias_deltas.insert(0, delta_bias)
 
-        return wake_deltas, hid_states[-1]
+        return wake_deltas, wake_bias_deltas, wake_visbias_delta, hid_states[-1]
 
     def sleep_phase(self, data):
         '''The last step of wake-sleep and contrastive wake-sleep. Returns sleep_deltas, a list of matrices by which the
-        the weights of the up net should be adjusted. Assumes DBN layers are binary stochastic layers.'''
+        the weights of the up net should be adjusted. Also returns sleep_bias_deltas. Assumes DBN layers are binary stochastic layers.'''
         #Get the states and probabilities of every layer after doing a top-down pass
         hid_states = self.top_down(data)
         hid_probs = []
@@ -92,6 +99,7 @@ class BN(object):
             hid_probs.append(layer.probs)
 
         sleep_deltas = []
+        sleep_bias_deltas = []
         #Iterate over each layer excluding top layer
         for i in range(self.downnet.numlayers -1):
             lower_state = hid_states[i+1]
@@ -99,21 +107,29 @@ class BN(object):
             upper_activity = hid_probs[i]
 
             delta = dot(lower_state.transpose(), (upper_state - upper_activity))/data.shape[0]
+            #Get bias deltas to update hidden biases in upnet
+            delta_bias = array([(upper_state - upper_activity).sum(0)/data.shape[0]])
             sleep_deltas.insert(0,delta)
+            sleep_bias_deltas.insert(0, delta_bias)
 
-        return sleep_deltas
+        return sleep_deltas, sleep_bias_deltas
 
     def wake_sleep(self, data, learning_rate):
         '''Combines wake and sleep phases'''
 
-        downnet_deltas, top_state = self.wake_phase(data)
-        upnet_deltas = self.sleep_phase(top_state) #The top state is the input for the top-down pass
+        downnet_deltas, downnet_hidbias_deltas, downnet_visbias_delta, top_state = self.wake_phase(data)
+        upnet_deltas, upnet_bias_deltas = self.sleep_phase(top_state) #The top state is the input for the top-down pass
         recons_error = square(data - self.upnet.layers[0].probs).sum()
         print 'BN Reconstruction Error', recons_error
+
+        self.downnet.layers[-1].bias += learning_rate*downnet_visbias_delta
         for i in range(len(downnet_deltas)):
             self.downnet.weights[i] += learning_rate*downnet_deltas[i]
+            self.downnet.layers[i+1].bias += learning_rate*downnet_hidbias_deltas[i]
+
         for i in range(len(upnet_deltas)):
             self.upnet.weights[i] += learning_rate*upnet_deltas[i]
+            self.upnet.layers[i+1].bias += learning_rate*upnet_bias_deltas[i]
         return recons_error
 
 class DBN(object):
@@ -151,8 +167,7 @@ class DBN(object):
     def contrastive_wake_sleep(self, data, K=1, learning_rate=1, rbm_data_func=None, bn_data_func=None): #Changed default learning rate, now it seems to be more useful
         '''Combines wake, CD, and sleep phases'''
 
-        downnet_deltas, top_state = self.bottom_layers.wake_phase(data)
-
+        downnet_deltas, downnet_hidbias_deltas, downnet_visbias_delta, top_state = self.bottom_layers.wake_phase(data)
         #Use samples of the top of the net as the input data of the top level RBM
         #Train top level RBM using CD-k, this will adjust the weight matrix of top RBM alone
         if rbm_data_func is not None:
@@ -163,11 +178,16 @@ class DBN(object):
         top_state = self.top_layer_rbm.gibbs_given_v(top_state, K)[0]
         if bn_data_func is not None:
             top_state = bn_data_func(top_state)
-        upnet_deltas = self.bottom_layers.sleep_phase(top_state)
+        upnet_deltas, upnet_bias_deltas = self.bottom_layers.sleep_phase(top_state)
 
         recons_error = square(data - self.bottom_layers.upnet.layers[0].probs).sum()
         print 'DBN Reconstruction Error', recons_error
+
+        self.bottom_layers.downnet.layers[-1].bias += learning_rate*downnet_visbias_delta
         for i in range(len(downnet_deltas)):
             self.bottom_layers.downnet.weights[i] += learning_rate*downnet_deltas[i]
+            self.bottom_layers.downnet.layers[i].bias += learning_rate*downnet_hidbias_deltas[i]
+
         for i in range(len(upnet_deltas)):
             self.bottom_layers.upnet.weights[i] += learning_rate*upnet_deltas[i]
+            self.bottom_layers.upnet.layers[i+1].bias += learning_rate*upnet_bias_deltas[i]
