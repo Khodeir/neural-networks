37a64ff1933673b052fd003115dd9e2fc0396f8b
Mohamed Khodeir <m.khodeir@mail.utoronto.ca>
2014-10-31 14:12:41 -0400

    Additions to metrics module.

--- metrics.py
+++ metrics.py
@@ -5,7 +5,8 @@ import pylab
 import backprop
 
 def learn_curve(network, training_input, training_target, validation_input, validation_target, epochs=50):
-    '''Plots the erros w.r.t the number of epochs of training. Assumes no regularisation.'''
+    '''Plots the erros w.r.t the number of epochs of training. Assumes no regularisation.
+    Returns two lists containing the training and validation errors.'''
     
     training_errors = []
     validation_errors = []
@@ -32,14 +33,82 @@ def learn_curve(network, training_input, training_target, validation_input, vali
     pylab.ylabel("Error")
     pylab.legend()
     pylab.show()
+
+    return training_errors, validation_errors
     
-def vary_data(network, data):
+def vary_data(network, training_input, training_target, validation_input, validation_target, epochs=5):
     '''Plots training and validation errors of a net w.r.t. the number of training
-    cases used in learning. Useful for determining if the issue is in the amount of data'''
+    cases used in learning. Useful for determining if the issue is in the amount of data
+    Returns two lists containing the training and validation errors'''
+    training_errors = []
+    validation_errors = []
+
+    num_training = len(training_input)
     
-def reg_parameter(network, data):
-    '''Plots training and validation errors of a net varying the regularisation parameter
-    over a set range. Useful for determining where network is underfitting and overfitting.'''
+    for percent in range(5, 105, 5): #Sweep through percentages from 5 to 100%
+        train_length = num_training*percent/100.0
+        validation_length = len(validation_input)*percent/100.0
+        network_copy = network
+    #Train mini batches of 10s
+        minibatches = [(training_input[i:i+10, :], training_target[i:i+10, :]) for i in range(0, int(train_length), 10)]
+        for epoch in range(epochs):
+            for batch in minibatches:
+                backprop.train(network_copy, batch[0], batch[1])
+
+        #Get errors for this percent of data
+        train_error = error(network_copy, training_input[0:train_length], training_target[0:train_length])
+        val_error = error(network_copy, validation_input[0:validation_length], validation_target[0:validation_length])
+        training_errors.append(train_error)
+        validation_errors.append(val_error)
+
+    #Plot the data
+    pylab.plot(range(5,105,5), training_errors, label="Training Error")
+    pylab.plot(range(5,105,5), validation_errors, label="Validation Error")
+    pylab.title("Training and Validation Errors w.r.t. % of Data used")
+    pylab.xlabel("Data Used (%)")
+    pylab.ylabel("Error After "+ str(epochs)+ " Epochs")
+    pylab.legend()
+    pylab.show()
+
+    return training_errors, validation_errors
+    
+def reg_parameter(network, training_input, training_target, validation_input, validation_target, sweep_range=(0.01, 20), epochs=10):
+    '''Plots training and validation errors of a net varying the regularisation parameter over a set range. Useful for determining 
+    where network is underfitting and overfitting. Sweep Range is a tuple, where the first value is the initial decay rate to test, 
+    and the second value is the number of parameters to test. Each parameter will be 1.2 times the one before it.
+    Returns two lists containing the training and validation errors'''
+    training_errors = []
+    validation_errors = []
+
+    num_training = len(training_input)
+    start_rate = sweep_range[0]
+    num_parameters = sweep_range[1]
+    decay_rates = [start_rate*(1.2**i) for i in range(num_parameters)] #Create a list of different regularisation parameters to try.
+    decay_rates.insert(0,0)
+    for rate in decay_rates:
+        #Train mini batches of 10s
+        new_net = network
+        minibatches = [(training_input[i:i+10, :], training_target[i:i+10, :]) for i in range(0, num_training, 10)]
+        for epoch in range(epochs):
+            for batch in minibatches:
+                backprop.train(new_net, batch[0], batch[1], rate)
+
+        #Get errors for this decay rate
+        train_error = error(new_net, training_input, training_target, rate)
+        val_error = error(new_net, validation_input, validation_target, rate)
+        training_errors.append(train_error)
+        validation_errors.append(val_error)
+
+    #Plot the data
+    pylab.plot(decay_rates, training_errors, label="Training Error")
+    pylab.plot(decay_rates,validation_errors, label="Validation Error")
+    pylab.title("Training and Validation Errors w.r.t. Regularisation Parameter")
+    pylab.xlabel("Regularisation Parameter")
+    pylab.ylabel("Error After 10 Epochs")
+    pylab.legend()
+    pylab.show()
+
+    return training_errors, validation_errors
 
 def learn_rate(network, data):
     '''Plots training and validation errors of a net when varying the learning rate. Useful
@@ -53,10 +122,9 @@ def vary_numlayers(network, data, sweep_range):
     '''Plots training and validation errors of a net by adding additional hidden layers of equal
     size, in the specified sweep range.'''
 
-def vary_sigmoid(network, data):
+def vary_sigmoid(network, training_input, training_target, validation_input, validation_target, epochs=50):
     '''Plots errors for a full sweep of varying the hidden layers to different types of sigmoid
     units.'''
-
 
 def error(net, data, T, decay_rate=0):
     '''Cross-entropy error of a net'''
@@ -68,7 +136,7 @@ def error(net, data, T, decay_rate=0):
     for layer in weights:
         regerror += (layer*layer).sum()  # Add the square of every weight in the network
     regerror *= (decay_rate/2)  # Normalise by decay constant
-    return error + regerror
+    return (error + regerror)/len(data) #Error normalised by number of training examples
 
 def gradcheck(network, layer, data, targets):
     '''Checks gradients obtained by backpropagation by getting gradients numerically'''
